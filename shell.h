ab4e16607550a7ac4cb0816478494ace85aa21ae
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2022-12-10 05:22:56 -0800

    header file

--- shell.h
+++ shell.h
@@ -1,103 +1,41 @@
 #ifndef SHELL_H
 #define SHELL_H
-
+#define _GNU_SOURCE
 #include <stdio.h>
-#include <wchar.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/wait.h>
 #include <sys/types.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
 
-/**
- * struct error_msg - An structure for each error message
- *
- * @ecode: error code
- * @msg: pointer to error message
- * @size: error message length.
- */
-typedef struct error_msg
-{
-	int ecode;
-	char *msg;
-	int  size;
-} error_msg_t;
+/* Shell functions shell_functions.c*/
+	void non_interact(char **av);
+	int interactive(char *av[], int count_exe, char **env);
+	int create_process(char *av[], int count_exe, char **env);
+	char *_getenv(char *name, char **env);
+	void print_env(char **env);
 
-/**
- * struct built_s - Builtings commands
- * @command: command name.
- * @f: function to call.
- *
- * Description: Longer description
- */
-typedef struct built_s
-{
-	char *command;
-	void (*f)(char **);
-} built_t;
+/* funcions search_path path_fuctions.c*/
+	char *validate_file(char *full_path, char *file);
 
-/**
- * struct history - An structure for each command readed
- *
- * @id_h: error code
- * @comms: Commands
- * @prev: Previous element
- * @next: Next element
- */
 
-typedef struct history
-{
-	unsigned int id_h;
-	char *comms;
-	struct history *prev;
-	struct history *next;
-} history_t;
+	/* Str functions  str_functions.c*/
+	char *_strcpy(char *dest, char *src);
+	int _strlen(const char *s);
+	char *_strcat(char *dest, char *src);
+	char *_strdup(char *str);
 
-/**
- * struct command_s - An structure for each command
- *
- * @command: command with arguments.
- * @next: pointer to next command.
- */
-typedef struct command_s
-{
-	char **command;
-	struct command_s *next;
-} command_t;
+	/* Str functions  strcmp_functions.c*/
+	int _strcmp(char *s1, char *s2);
+	int _strncmp(const char *s1, const char *s2, size_t n);
 
-/* Shell functions */
-command_t **_prompt(char *, char *);
-int _fork(char *, command_t *, char *, char **);
-int _stat(char *, char *);
-int _exec(char *, char **, char **);
-
-/* Utilities */
-char *read_line(void);
-
-size_t _strlen(char *str);
-command_t *_parser_cmd(char *, char *);
-size_t _parser_arg(char *, char **, size_t *);
-void print_char_pointer_arr(char **, size_t);
-int add_nodeint(history_t **head, char *str);
-char *_strdup(char *str);
-void free_listint(history_t *head);
-void print_listint(const history_t *);
-char *find_path(char **);
-char *_strstr(char *haystack, char *needle);
-void print_env(char **);
-char *_which(char *p_rec, char *first_arg);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-int _strcmp(char *s1, char *s2);
-void _exit_func(char **);
-int verif_built_comm(char *str, char **env);
-
-/* Error handler */
-void error_handler(char *, int);
-void error_handler_set_default(int, char *);
-
-/* Command Utilities */
-command_t *new_cmd_node(char *);
-void add_tok_to_cmd(char *, command_t *, size_t, char *);
+	/* Memory allocate functions */
+	char *_memset(char *s, char b, unsigned int n);
+	void *_calloc(unsigned int nmemb, int size);
+	unsigned int lenght_array(char **p);
 
 #endif
